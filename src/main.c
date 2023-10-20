d2daed99870e508f7ae5c9eb5f4b0ce3ad5459f2
bcox484 <bcox484@outlook.com>
2023-10-20 12:11:26 -0600

    more compatible gtk4 version

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
-    app = gtk_application_new("tic.tac.toe", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("tic.tac.toe", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
     status = g_application_run(G_APPLICATION(app), argc, argv);
